5891f7a7907c53f3281f7fc83ae9fba64ea0905f
Vladyslav <tokhtarits@atbmarket.com>
2023-07-07 19:27:30 +0300

    Added switch (Market)

--- Shares/Copy Market Shares.ps1
+++ Shares/Copy Market Shares.ps1
@@ -8,79 +8,88 @@
 
 $serverName = read-host 'Введите имя сервера с которого необходимо скопировать сетевые директории'
 $remoteShares = Get-SmbShare -CimSession $serverName #Получаем список шар на удаленном сервере
+$copyType = Read-Host 'Введите какие сетевые директории необходимо скопировать (All , Market)'
 $error_flag = $false
+
 # Write-Output $remoteShares.Name
 
-#Составляем список сетевых директорий которые будут скопированы
-Write-Host -fore Green 'Будут скопированны следующие сетевые директории:'
-foreach ($remoteShare in $remoteShares) {  
-    if ( $remoteShare.Name -like '*pocketbot*' -Or $remoteShare.Name -like '*repl*' -Or $remoteShare.Name -like '*market*' ) {
-        Write-Host -fore Green $remoteShare.Path   
-    }
-}
-Write-Host '==================================================================================='
+switch ($copyType)
+{
 
-# Обрабатываем каждую директорию отдельно
-foreach ($remoteShare in $remoteShares) { 
-    
-    # Фильтруем сетевые директории по ключевым фразам, которые должны содержаться их в именах  
-    if ( $remoteShare.Name -like '*pocketbot*' -Or $remoteShare.Name -like '*repl*' -Or $remoteShare.Name -like '*market*' ) { 
+    ('Market') {
 
-            # Проверяем и копируем отсутствующие директории
-            if (-not (Test-Path -Path $remoteShare.Path)) {
-                Write-Host -fore Yellow 'Копирую директорию' $remoteShare.Path 'на локальный компьютер...'
-                $remotePath = "\\$serverName\" + $remoteShare.Name
-                $localPath = $remoteShare.Path
-                robocopy $remotePath $localPath /COPY:S /E /IS /DCOPY:T
-
-                # Проверка успешности копирования директории
-                if ( Test-Path -Path $remoteShare.Path ) { Write-Host -fore Green 'Директория' $remoteShare.Path 'успешно скопирована на локальный компьютер!'
-                } else { 
-                    Write-Host -fore Red 'Директория' $remoteShare.Path 'не была скопирована на локальный компьютер! Проверьте ошибки выполнения модуля robocopy.' 
-                    $error_flag = $true
-                        }
-            } else { Write-Host -fore Green 'Директория' $remoteShare.Path 'уже существует на локальном компьютере! Никаких действий не требуется.' }
-
-            # Получаем информацию о доступах к сетевым директориям на удаленном компьютере
-            $remoteShareAccessList = Get-SmbShareAccess -Name $remoteShare.Name -CimSession $serverName
-            
-            # Получаем список сетевых директорий на локальном компьютере
-            $localShareList = Get-SmbShare
-
-            # Проверяем наличие такой же сетевой директории на локальном компьютере
-            $flag = $false
-            foreach ($localShare in $localShareList.Name) {
-                if ( $localShare -eq $remoteShare.Name ) { $flag = $true }
+            #Составляем список сетевых директорий которые будут скопированы
+            Write-Host -fore Green 'Будут скопированны следующие сетевые директории:'
+            foreach ($remoteShare in $remoteShares) {  
+                if ( $remoteShare.Name -like '*pocketbot*' -Or $remoteShare.Name -like '*repl*' -Or $remoteShare.Name -like '*market*' ) {
+                    Write-Host -fore Green $remoteShare.Path   
+                }
             }
-   
+            Write-Host '==================================================================================='
 
-            if ( $flag -eq $true ) { Write-Host -fore Green 'Сетевая директория' $remoteShare.Name 'уже существует на локальном компьютере! Никаких действий не требуется.'
-            } else { 
-                    if (-not (Test-Path -Path $remoteShare.Path)) {
-                        Write-Host -fore Red 'Сетевая директория' $remoteShare.Name 'не создана на локальном компьютере по причине отсутствия директории' $remoteShare.Path
-                        $error_flag = $true
-                    } else {
-                        # Создаем такую же сетевую директорию на локальномм компьютере
-                        Write-Host -fore Yellow 'Создаю сетевую директорию' $remoteShare.Name 'на локальном компьютере и назначаю права:'
-                        New-SmbShare -Name $remoteShare.Name -Path $remoteShare.Path | Out-Null
+            # Обрабатываем каждую директорию отдельно
+            foreach ($remoteShare in $remoteShares) { 
                 
-                        # Отдельно берем каждый объект с правами на сетевую директорию и задаем такой же на локальную директорию      
-                        foreach ($remoteShareAccess in $remoteShareAccessList) {                        
-                            Grant-SmbShareAccess -Name $remoteShareAccess.Name -AccountName $remoteShareAccess.AccountName -AccessRight $remoteShareAccess.AccessRight -Force | Out-Null
-                            Write-Host -fore Yellow 'Назначаю права' $remoteShareAccess.AccessRight 'объекту' $remoteShareAccess.AccountName
+                # Фильтруем сетевые директории по ключевым фразам, которые должны содержаться их в именах  
+                if ( $remoteShare.Name -like '*pocketbot*' -Or $remoteShare.Name -like '*repl*' -Or $remoteShare.Name -like '*market*' ) { 
+
+                        # Проверяем и копируем отсутствующие директории
+                        if (-not (Test-Path -Path $remoteShare.Path)) {
+                            Write-Host -fore Yellow 'Копирую директорию' $remoteShare.Path 'на локальный компьютер...'
+                            $remotePath = "\\$serverName\" + $remoteShare.Name
+                            $localPath = $remoteShare.Path
+                            robocopy $remotePath $localPath /COPY:S /E /IS /DCOPY:T
+
+                            # Проверка успешности копирования директории
+                            if ( Test-Path -Path $remoteShare.Path ) { Write-Host -fore Green 'Директория' $remoteShare.Path 'успешно скопирована на локальный компьютер!'
+                            } else { 
+                                Write-Host -fore Red 'Директория' $remoteShare.Path 'не была скопирована на локальный компьютер! Проверьте ошибки выполнения модуля robocopy.' 
+                                $error_flag = $true
+                                    }
+                        } else { Write-Host -fore Green 'Директория' $remoteShare.Path 'уже существует на локальном компьютере! Никаких действий не требуется.' }
+
+                        # Получаем информацию о доступах к сетевым директориям на удаленном компьютере
+                        $remoteShareAccessList = Get-SmbShareAccess -Name $remoteShare.Name -CimSession $serverName
+                        
+                        # Получаем список сетевых директорий на локальном компьютере
+                        $localShareList = Get-SmbShare
+
+                        # Проверяем наличие такой же сетевой директории на локальном компьютере
+                        $flag = $false
+                        foreach ($localShare in $localShareList.Name) {
+                            if ( $localShare -eq $remoteShare.Name ) { $flag = $true }
                         }
-                    
-                    
-                    Write-Host -fore Green 'Сетевая директория' $remoteShare.Name 'успешно создана на локальном компьютере!'
-
-                    }
- 
-              }
             
-    Write-Host '==================================================================================='
 
+                        if ( $flag -eq $true ) { Write-Host -fore Green 'Сетевая директория' $remoteShare.Name 'уже существует на локальном компьютере! Никаких действий не требуется.'
+                        } else { 
+                                if (-not (Test-Path -Path $remoteShare.Path)) {
+                                    Write-Host -fore Red 'Сетевая директория' $remoteShare.Name 'не создана на локальном компьютере по причине отсутствия директории' $remoteShare.Path
+                                    $error_flag = $true
+                                } else {
+                                    # Создаем такую же сетевую директорию на локальномм компьютере
+                                    Write-Host -fore Yellow 'Создаю сетевую директорию' $remoteShare.Name 'на локальном компьютере и назначаю права:'
+                                    New-SmbShare -Name $remoteShare.Name -Path $remoteShare.Path | Out-Null
+                            
+                                    # Отдельно берем каждый объект с правами на сетевую директорию и задаем такой же на локальную директорию      
+                                    foreach ($remoteShareAccess in $remoteShareAccessList) {                        
+                                        Grant-SmbShareAccess -Name $remoteShareAccess.Name -AccountName $remoteShareAccess.AccountName -AccessRight $remoteShareAccess.AccessRight -Force | Out-Null
+                                        Write-Host -fore Yellow 'Назначаю права' $remoteShareAccess.AccessRight 'объекту' $remoteShareAccess.AccountName
+                                    }
+                                
+                                
+                                Write-Host -fore Green 'Сетевая директория' $remoteShare.Name 'успешно создана на локальном компьютере!'
+
+                                }
+            
+                        }
+                        
+                Write-Host '==================================================================================='
+
+                }
+
+            }
+
+            if ($error_flag -eq $true) { Write-Host -fore Red 'ВНИМАНИЕ! ПРИ ВЫПОЛНЕНИИ СКРИПТА БЫЛИ ОБНАРУЖЕНЫ ОШИБКИ, ПРОВЕРЬТЕ ВЫВОД!' }
+        }
     }
-
-}
-
-if ($error_flag -eq $true) { Write-Host -fore Red 'ВНИМАНИЕ! ПРИ ВЫПОЛНЕНИИ СКРИПТА БЫЛИ ОБНАРУЖЕНЫ ОШИБКИ, ПРОВЕРЬТЕ ВЫВОД!' }
